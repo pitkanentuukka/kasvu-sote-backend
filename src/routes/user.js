4b72d58523aa0055839009f3a1ab058d4cbaa4b1
pitkanentuukka <pitkanentuukka@gmail.com>
2020-11-20 17:41:58 +0200

    started refactoring sql connections & queries to use async/await & single connection pool, still a truckload of work to do

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
-const { config } = require('../config')
+const pool = require('../db/mysql.js').pool
+
+//const { config } = require('../config')
 const {getRoleAndId} = require('../cookie-helper')
 const nodemailer = require('nodemailer')
 
@@ -11,36 +13,37 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
 
-router.post('/login', bodyParser(), (req, res) => {
-
+router.post('/login', bodyParser(), async (req, res) => {
+  
   const email = req.body.email
   const password = req.body.password
   if (email && password) {
     const sql = "select * from user where email = ?"
     const inserts = email;
-    config.sql_pool().getConnection((err, connection) => {
-      connection.query(sql, inserts, (error, results, fields) =>{
-        if (results.length === 0) {
-          res.status(403).json({"message": "invalid username or password"}).end()
-        } else {
-          bcrypt.compare(password, results[0].password, (bcerr, bcres)=> {
-            if (bcres) {
-              const userId = results[0].user_id;
-              const role = results[0].role;
-              const payload = { email, role, userId }
-              const token = jwt.sign(payload, process.env.JWT_KEY, {
-                expiresIn: '365d'}
-              )
-              res.status(200).cookie('token', token, {httpOnly : true})
-              res.json({"userid": results[0].user_id, "role": results[0].role})
-              res.end()
-            } else {
-              res.status(403).json({"msg": "invalid username or password"}).end()
-            }
-          })
-        }
-      })
-    })
+    try {
+      const results = await pool.query(sql, inserts)
+      if (!results[0][0]) {
+        res.status(403).json({"message": "invalid username or password"}).end()
+      } else {
+        bcrypt.compare(password, results[0][0].password, (bcerr, bcres)=> {
+          if (bcres) {
+            const userId = results[0][0].user_id;
+            const role = results[0][0].role;
+            const payload = { email, role, userId }
+            const token = jwt.sign(payload, process.env.JWT_KEY, {
+              expiresIn: '365d'}
+            )
+            res.status(200).cookie('token', token, {httpOnly : true})
+            res.json({"userid": results[0].user_id, "role": results[0].role})
+            res.end()
+          } else {
+            res.status(403).json({"msg": "invalid username or password"}).end()
+          }
+        })
+      }
+    } catch (e) {
+      res.status(500).json(e).end()
+    }
   } else {
     res.status(400).json({"msg": "missing email or password"}).end()
   }
