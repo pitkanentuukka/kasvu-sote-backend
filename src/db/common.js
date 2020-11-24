8612c49909cb0f3955c9c88815385b19b51d0eea
Katariina Forsblom <katfor@ad.digitalentshelsinki.fi>
2020-11-24 11:08:39 +0200

    Added new procedures to handle database requests and modified existing prodecures to call these database handling procedures.

---
+++ src/db/common.js
@@ -0,0 +1,32 @@
+const pool = require('../db/mysql.js').pool
+
+exports.getModules = async () => {
+  try {
+    results = await pool.query("select * from module")
+    return results[0]
+  }
+  catch (error) {
+    throw (error)
+  }
+}
+
+exports.getGategories = async (id) => {
+  try {
+    results = await pool.query("select * from category where module_id = ?", id)
+    return results[0]
+  }
+  catch (error) {
+    throw (error)
+  }
+}
+
+exports.getCriterias = async (id) => {
+  try {
+    results = await pool.query("select * from criteria where category_id = ?", id)
+    return results[0]
+  }
+  catch (error) {
+    throw (error)
+  }
+}
+
--- src/routes/common.js
+++ src/routes/common.js
@@ -6,48 +6,69 @@ const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
+const pool = require('../db/mysql.js').pool
+const user = require('../db/user.js')
+const common = require('../db/common.js')
 
-
-router.get("/modules", cors(), (req, res) => {
-  const sql = "SELECT * FROM module"
-  config.sql_pool().getConnection((err, connection) => {
-    connection.query(sql, (err, results, fields) => {
-      connection.release()
-      if (err) {
-        res.status(500).end()
-      }else if (results.length > 0) {
-
-        res.status(200).json(results).end()
-      } else {
-        res.status(200).json({"msg": "no modules found"}).end()
-      }
-
-    })
-  })
+router.get("/modules", cors(), async (req, res) => {
+  try {
+    const results = await common.getModules()
+    if (results[0].length === 0) {
+      res.status(403).json({"message": "no modules found"}).end()
+    } else if (results.length > 0) {
+      res.status(200).json(results).end()
+    } else {
+      res.status(200).json({"message": "no modules found"}).end()
+    }
+   
+  } catch (error) {
+    res.status(500).json(error).end
+  }
 })
 
-router.get("/categories/:id", cors(), (req, res) => {
+router.get("/categories/:id", cors(), async (req, res) => {
   if (!req.params.id) {
+        // bad request
+        res.status(400).end()
+  } else {
+    try {
+      const inserts = [req.params.id]
+      const results =  await common.getGategories(inserts)
+      if (results[0].length === 0) {
+        res.status(403).json({"message": "no categories found"}).end()
+      } else if (results.length > 0) {
+        res.status(200).json(results).end()
+      } else {
+        res.status(200).json({"message": "no categories found"}).end()
+      }
+    }
+    catch (error)
+    {
+      res.status(500).json(error).end
+    }
+  }
+})
+
+router.get("/criteria/:id", cors(), async (req, res) => {
+  if (!req.params.id)
+  {
     // bad request
     res.status(400).end()
   } else {
-    const inserts = [req.params.id]
-    const sql = "SELECT * FROM category WHERE module_id = ?"
-    config.sql_pool().getConnection((err, connection) => {
-
-      connection.query(sql, inserts, (err, results, fields) => {
-        connection.release()
-        if (err) {
-          res.status(500).end()
-        }else if (results.length > 0) {
-
-          res.status(200).json(results).end()
-        } else {
-          res.status(200).json({"msg": "no categories found"}).end()
-        }
-
-      })
-    })
+    try {
+      const inserts = [req.params.id]
+      const result = await common.getCriterias(inserts)
+      if (results[0].length === 0) {
+        res.status(403).json({"message": "no criterias found"}).end()
+      } else if (results.length > 0) {
+        res.status(200).json(results).end()
+      } else {
+          res.status(200).json({"message": "no criterias found"}).end()
+      }
+    }
+    catch (error) {
+      res.status(500).json(error).end
+    }
   }
 })
 
