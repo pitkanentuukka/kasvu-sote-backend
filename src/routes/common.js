ac476f074ed6ce74fa30d55066db8c9ec32cba2c
pitkanentuukka <pitkanentuukka@gmail.com>
2020-12-14 13:47:02 +0200

    fix bug when there's no results

--- src/routes/common.js
+++ src/routes/common.js
@@ -13,14 +13,11 @@ const common = require('../db/common.js')
 router.get("/modules", cors(), async (req, res) => {
   try {
     const results = await common.getModules()
-    if (results[0].length === 0) {
-      res.status(403).json({"message": "no modules found"}).end()
+    if (results.length === 0) {
+      res.status(200).json({"message": "no modules found"}).end()
     } else if (results.length > 0) {
       res.status(200).json(results).end()
-    } else {
-      res.status(200).json({"message": "no modules found"}).end()
     }
-   
   } catch (error) {
     res.status(500).json(error).end
   }
@@ -34,16 +31,16 @@ router.get("/categories/:id", cors(), async (req, res) => {
     try {
       const inserts = [req.params.id]
       const results =  await common.getGategories(inserts)
-      if (results[0].length === 0) {
-        res.status(403).json({"message": "no categories found"}).end()
+
+      if (results.length === 0) {
+        res.status(200).json({"message": "no categories found"}).end()
       } else if (results.length > 0) {
         res.status(200).json(results).end()
-      } else {
-        res.status(200).json({"message": "no categories found"}).end()
       }
     }
     catch (error)
     {
+      console.log(error);
       res.status(500).json(error).end
     }
   }
@@ -58,12 +55,10 @@ router.get("/criteria/:id", cors(), async (req, res) => {
     try {
       const inserts = [req.params.id]
       const results = await common.getCriterias(inserts)
-      if (results[0].length === 0) {
-        res.status(403).json({"message": "no criterias found"}).end()
+      if (results.length === 0) {
+        res.status(200).json({"message": "no criterias found"}).end()
       } else if (results.length > 0) {
         res.status(200).json(results).end()
-      } else {
-          res.status(200).json({"message": "no criterias found"}).end()
       }
     }
     catch (error) {
